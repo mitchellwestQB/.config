336ce85ee431cfb6dfc4e23f6b06ed7253047f4f
mitchellwestQB <mitchell_west@mckinsey.com>
2024-12-26 17:12:55 +1000

    added option movement

--- nvim/init.lua
+++ nvim/init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -190,12 +190,6 @@ end, { desc = '[t]oggle [d]iagnostics' })
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
@@ -207,6 +201,17 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- Custom keybinds
 vim.keymap.set('x', '<leader>p', '"_dP')
+
+-- translate
+vim.keymap.set('n', '<leader>T', '<cmd>Translate EN<CR>', { desc = '[T]ranslate EN' })
+
+-- navigation
+vim.keymap.set('n', '<M-h>', 'b')
+vim.keymap.set('n', '<M-l>', 'w')
+vim.keymap.set('n', '<M-j>', '}')
+vim.keymap.set('n', '<M-k>', '{')
+-- vim.keymap.set('n', '<M-right>', 'w')
+-- vim.keymap.set('n', '<M-left>', 'b')
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -919,6 +924,7 @@ require('lazy').setup({
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  { 'Bekaboo/dropbar.nvim' },
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -1,20 +1,93 @@
 return {
   require 'custom.plugins.dashboard',
   require 'custom.plugins.lualine',
-  require 'custom.plugins.avante',
+  -- require 'custom.plugins.avante',
   require 'custom.plugins.copilot',
   require 'custom.plugins.fugitive',
   require 'custom.plugins.lspkind',
   require 'custom.plugins.oil',
   {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+
+      -- basic telescope configuration
+      local conf = require('telescope.config').values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require('telescope.pickers')
+          .new({}, {
+            prompt_title = 'Harpoon',
+            finder = require('telescope.finders').new_table {
+              results = file_paths,
+            },
+            previewer = conf.file_previewer {},
+            sorter = conf.generic_sorter {},
+          })
+          :find()
+      end
+      vim.keymap.set('n', '<C-e>', function()
+        toggle_telescope(harpoon:list())
+      end, { desc = 'Open harpoon window' })
+
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end)
+      -- vim.keymap.set('n', '<C-e>', function()
+      --   harpoon.ui:toggle_quick_menu(harpoon:list())
+      -- end)
+
+      vim.keymap.set('n', '<C-h>', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', '<C-j>', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', '<C-k>', function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set('n', '<C-l>', function()
+        harpoon:list():select(4)
+      end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set('n', '<C-S-P>', function()
+        harpoon:list():prev()
+      end)
+      vim.keymap.set('n', '<C-S-N>', function()
+        harpoon:list():next()
+      end)
+    end,
+  },
+  {
+    'xiyaowong/transparent.nvim',
+    config = function()
+      require('transparent').setup {
+        extra_groups = {
+          'NormalFloat', -- plugins which have float panel such as Lazy, Mason, LspInfo
+          'NvimTreeNormal',
+        },
+      }
+    end,
+  },
+  {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
+    flavour = 'macchiato',
     integrations = {
       cmp = true,
       dashboard = true,
       gitsigns = true,
-      nvimtree = true,
+      -- nvimtree = true,
+      neotree = true,
       treesitter = true,
       notify = false,
       mason = true,
@@ -32,6 +105,7 @@ return {
       vim.cmd 'colorscheme catppuccin-macchiato'
     end,
   },
+  { 'uga-rosa/translate.nvim' },
 }
 
 -- {
