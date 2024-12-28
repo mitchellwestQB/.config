eefa4d7734a7032d943f9fdd6be7e5e0a177d86d
mitchellwestQB <mitchell_west@mckinsey.com>
2024-12-28 16:49:04 +1000

    updated lualine

--- nvim/lua/plugins/ui/lualine.lua
+++ nvim/lua/plugins/ui/lualine.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-lualine/lualine.nvim',
   opts = {
-    theme = 'catppuccin-mocha',
+    theme = 'catppuccin',
     options = {
       component_separators = ' ',
       section_separators = { left = '', right = '' },
